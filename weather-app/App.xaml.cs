e6a32575224188fef807541220b3ff79936622cd
HaHtn <htnganha241@gmail.com>
2023-03-22 00:16:46 +0700

    Add project files.

---
+++ weather-app/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace weather_app;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+		VersionTracking.Track();
+		if(VersionTracking.IsFirstLaunchEver == true)
+		{
+            MainPage = new WelcomePage();
+        }
+		else
+		{
+			MainPage = new WeatherPage();
+		}
+	}
+}
---
+++ weather-app/Services/ApiService.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using weather_app.Models;
+
+namespace weather_app.Services
+{
+    public static class ApiService
+    {
+        public static async Task<Root> GetWeather(double latitude, double longitude)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(string.Format("https://api.openweathermap.org/data/2.5/forecast?lat={0}&lon={1}&units=metric&appid=7db7f7f1454b662870a48eb474a823f9", latitude, longitude));
+            return JsonConvert.DeserializeObject<Root>(response);
+        }
+        public static async Task<Root> GetWeatherByCity(string city)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(string.Format("https://api.openweathermap.org/data/2.5/forecast?q={0}&units=metric&appid=7db7f7f1454b662870a48eb474a823f9", city));
+            return JsonConvert.DeserializeObject<Root>(response);
+        }
+    }
+}
